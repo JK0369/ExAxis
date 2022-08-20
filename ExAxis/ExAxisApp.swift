d7cb43cdf2650fd20216d0623c0799278f1cab94
jake <palatable7@naver.com>
2022-08-20 15:04:41 +0900

    Initial Commit

---
+++ ExAxis/ExAxisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExAxisApp.swift
+//  ExAxis
+//
+//  Created by 김종권 on 2022/08/20.
+//
+
+import SwiftUI
+
+@main
+struct ExAxisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
