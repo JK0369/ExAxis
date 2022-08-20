563d85e2e0e7986e73d7ff98262ad5b9a7053847
jake <palatable7@naver.com>
2022-08-20 15:20:58 +0900

    implement

--- ExAxis/ContentView.swift
+++ ExAxis/ContentView.swift
@@ -7,15 +7,53 @@
 
 import SwiftUI
 
+enum NotifyMeAboutType {
+  case directMessages
+  case mentions
+  case anything
+}
+
+enum ProfileImageSize {
+  case large
+  case medium
+  case small
+}
+
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  @State var notifyMeAbout = NotifyMeAboutType.directMessages
+  @State var playNotificationSounds = false
+  @State var profileImageSize = ProfileImageSize.large
+  @State var sendReadReceipts = false
+  
+  var body: some View {
+    NavigationView {
+      Form {
+        Section(header: Text("Notifications")) {
+          Picker("Notify Me About", selection: $notifyMeAbout) {
+            Text("Direct Messages").tag(NotifyMeAboutType.directMessages)
+            Text("Mentions").tag(NotifyMeAboutType.mentions)
+            Text("Anything").tag(NotifyMeAboutType.anything)
+          }
+          Toggle("Play notification sounds", isOn: $playNotificationSounds)
+          Toggle("Send read receipts", isOn: $sendReadReceipts)
+        }
+        Section(header: Text("User Profiles")) {
+          Picker("Profile Image Size", selection: $profileImageSize) {
+            Text("Large").tag(ProfileImageSize.large)
+            Text("Medium").tag(ProfileImageSize.medium)
+            Text("Small").tag(ProfileImageSize.small)
+          }
+          Button("Clear Image Cache") {}
+        }
+      }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
+
+
